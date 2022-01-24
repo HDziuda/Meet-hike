c8bafdf0edbdb97cd7bcb334ab92496e1cc4e33b
Hubert Dziuda <hubert@dziuda.it>
2022-01-24 17:38:15 +0100

    Removed auto mute and auto cam off features

--- src/content.js
+++ src/content.js
@@ -1,16 +1,3 @@
-const keyDescriptions = {
-  keyD: {
-    key: "d",
-    code: "KeyD",
-    keyCode: 68,
-  },
-  keyE: {
-    key: "e",
-    code: "KeyE",
-    keyCode: 69,
-  },
-};
-
 const iconSize = 19;
 
 const uncheckedIcon = `
@@ -22,52 +9,6 @@ const checkedIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="${iconSize}"
 <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
 <path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.235.235 0 0 1 .02-.022z"/>
 </svg>`;
-
-const isMac = () => navigator.userAgent.indexOf("Mac OS X") != -1;
-console.log(isMac());
-
-const simulateKeyPress = (keyDescription) => {
-  document.body.dispatchEvent(
-    new KeyboardEvent("keydown", {
-      isTrusted: true,
-      altKey: false,
-      bubbles: true,
-      cancelBubble: false,
-      cancelable: true,
-      charCode: 0,
-      composed: true,
-      ctrlKey: !isMac(),
-      currentTarget: null,
-      defaultPrevented: true,
-      detail: 0,
-      eventPhase: 0,
-      isComposing: false,
-      location: 0,
-      metaKey: isMac(),
-      ...keyDescription,
-    })
-  );
-};
-
-const toggleMic = () => {
-  simulateKeyPress(keyDescriptions.keyD);
-};
-const toggleCam = () => {
-  simulateKeyPress(keyDescriptions.keyE);
-};
-
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (sender.id == chrome.runtime.id) {
-    switch (request.action) {
-      case "toggleMic":
-        toggleMic();
-        break;
-      case "toggleCam":
-        toggleCam();
-        break;
-    }
-  }
-});
 
 const participantIndex = (participant) => {
   const pName = participant.querySelector("span").innerHTML;
@@ -154,31 +95,3 @@ window.addEventListener("transitioncancel", (event) => {
 window.addEventListener("focus", (event) => {
   setTimeout(createCheckboxes, 10);
 });
-
-// Object.keys(window).forEach((key) => {
-//   if (/^on/.test(key)) {
-//     if (
-//       "transitionrun" == key.slice(2) ||
-//       "mouseup" == key.slice(2) ||
-//       "focus" == key.slice(2) ||
-//       "deviceorientationabsolute" == key.slice(2) ||
-//       "pointerrawupdate" == key.slice(2) ||
-//       "transitionstart" == key.slice(2) ||
-//       "transitionend" == key.slice(2) ||
-//       "mouseout" == key.slice(2) ||
-//       "mousemove" == key.slice(2) ||
-//       "mouseover" == key.slice(2) ||
-//       "pointerout" == key.slice(2) ||
-//       "pointerover" == key.slice(2) ||
-//       "blur" == key.slice(2) ||
-//       "pointermove" == key.slice(2) ||
-//       "animationiteration" == key.slice(2) ||
-//       "animationstart" == key.slice(2)
-//     )
-//       return;
-//     window.addEventListener(key.slice(2), (event) => {
-//       console.log("fired: " + key.slice(2));
-//       console.log(event);
-//     });
-//   }
-// });
